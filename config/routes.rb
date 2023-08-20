34139262bf53bf3626ea478ed2e0a0288a2c9975
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-08-20 13:45:59 +0000

    [devise]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations, :passwords]  ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
