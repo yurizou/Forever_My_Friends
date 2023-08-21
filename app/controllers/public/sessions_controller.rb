4adbc4dd0a4f6f6773d8218b6b957c0d9bd72e36
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-08-21 13:26:05 +0000

    [devise]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :user_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +25,16 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-end
+  
+  protected
+  
+  def user_status
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.valid_password?(params[:user][:password])
+      if @user.is_delete == true
+        redirect_to new_user_registration_path
+      end
+    end
+   end
+  end
