863c59b3a1824591d87d017a84334173af2c46e9
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-08-21 13:06:44 +0000

    [devise]

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords]  ,controllers: {
     sessions: "admin/sessions"
   }
-  
-  
+
+
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
