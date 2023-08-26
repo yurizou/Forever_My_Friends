ccffeb7e3ee9ed2559d2e0d97d0210c08b8b07a0
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-08-26 05:33:44 +0000

    [login]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Admin.create!(
+  email: 'daiki@gmail.com',
+  password: 'daiki1209'
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
