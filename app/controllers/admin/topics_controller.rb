335cd0282e86bf53232c81c4bf87ce71f53c017c
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-09-01 14:15:36 +0000

    [tipic]

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -2,26 +2,30 @@ class Admin::TopicsController < ApplicationController
   def new
     @topic = Topic.new
   end
-  
+
   def create
-    
+
   end
-  
+
   def index
     @topics = Topic.all
   end
-  
+
   def edit
-    
+
   end
-  
+
   def show
-    
+
   end
-  
+
   def update
-    
+
   end
-  
-  
+
+ def destroy
+   topics =Topic.find(params[:id])
+   topics.destroy
+   redirect_to admin_topics_path
+ end
 end
