92a9bb7dbd0b67494dc2d1ade8b3300231e8b912
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-09-03 13:11:33 +0000

    [search]

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,12 +2,13 @@ class SearchesController < ApplicationController
    before_action :authenticate_user!
 
   def search
-    @range = params[:range]
+    #@range = params[:range]
+    #binding.pry
 
-    if @range == "User"
-      @users = Friend.looks(params[:search], params[:word])
-    else
-      @books = Plan.looks(params[:search], params[:word])
-    end
+    #if @range == "User"
+      @friends = Friend.where("nickname LIKE ? OR last_name LIKE ?", "%#{params[:word]}%", "%#{params[:word]}%")
+    #else
+      #@books = Plan.looks(params[:search], params[:word])
+    #end
   end
 end
